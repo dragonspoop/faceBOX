056dbb0dc269f09e031dd9d6a6cbb249e72e5262
dragonspoop <137092078+dragonspoop@users.noreply.github.com>
2024-06-22 16:11:52 +0530

    changes

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
 
-const meeting = ({params}: { params: { id: string } }) => {
+import { useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useParams } from "next/navigation";
+import { Loader } from "lucide-react";
+import MeetingSetup from "@/components/MeetingSetup";
+import MeetingRoom from "@/components/MeetingRoom";
+import { useGetCallById } from "@/hooks/useGetCallById";
+
+const MeetingPage = ({ params: { id } }: { params: { id: string } }) => {
+  // const { id } = useParams();
+  const { isLoaded, user } = useUser();
+  const { call, isCallLoading } = useGetCallById(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  // if (!call)
+  //   return (
+  //     <p className="text-center text-3xl font-bold text-white">
+  //       Call Not Found
+  //     </p>
+  //   );
+
+  // const notAllowed =
+  //   call.type === "invited" &&
+  //   (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  // if (notAllowed)
+  //   return <Alert title="You are not allowed to join this meeting" />;
+
   return (
-    <div>meeting room: #{params.id}</div>
-  )
-}
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
+};
 
-export default meeting
+export default MeetingPage;
